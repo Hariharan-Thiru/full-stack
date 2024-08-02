4693539fab08194cf386f78c5255a76fae016631
Hariharan-Thiru <hariharanthiru2004@gmail.com>
2024-08-02 14:10:10 +0530

    empproject

--- angular/employee/src/app/app.component.ts
+++ angular/employee/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { employee } from './model/employee';
+import { EmployeeService } from './employee.service';
 
 @Component({
   selector: 'app-root',
@@ -9,14 +10,43 @@ import { employee } from './model/employee';
 export class AppComponent {
   title = 'employee';
   employee: employee;
+  result : string;
+  flag : boolean;
+  employeeArr: employee[];
 
-  constructor(){
+  constructor(private service : EmployeeService){
     this.employee=new employee();
+    this.result="";
+    this.employeeArr=[];
+    this.flag=false;
   }
   insertEmployee(data:any){
-    this.employee.empId=data.empId;
+    this.employee.id=data.empId;
     this.employee.empName=data.empName;
     this.employee.empSalary=data.empSalary;
-    alert(data.empId+" "+data.empName+" "+data.empSalary);
+
+    this.result=this.service.insertEmployee(this.employee);
+  }
+
+  updateEmployee(data:any){
+    this.employee.id=data.empId;
+    this.employee.empName=data.empName;
+    this.employee.empSalary=data.empSalary;
+
+    this.result=this.service.updateEmployee(this.employee);
+  }
+
+  deleteEmployee(data:any){
+    this.result=this.service.deleteEmployee(data.empId);
+  }
+
+  findAllEmployee(){
+    this.employeeArr= this.service.findAllEmployee();
+    this.flag=true;
+  }
+
+  findEmployee(data:any){
+    this.employee= this.service.findEmployee(data.empId);
+    this.result=this.employee.id+ " "+ this.employee.empName+" "+this.employee.empSalary;
   }
 }
--- angular/employee/src/app/employee.service.ts
+++ angular/employee/src/app/employee.service.ts
@@ -1,9 +1,45 @@
 import { Injectable } from '@angular/core';
+import { employee } from './model/employee';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  constructor() { }
+  url:string;
+  employeeArr : employee[];
+  employee : employee;
+
+  constructor(private http: HttpClient) {
+    this.url="http://localhost:3004/employees";
+    this.employeeArr =[];
+    this.employee=new employee();
+
+   }
+
+   insertEmployee(employee : employee){
+    this.http.post<employee>(this.url, employee).subscribe();
+    return "Employee Details Added";
+   }
+
+   updateEmployee(employee : employee){
+    this.http.put<employee>(this.url+"/"+employee.id,employee).subscribe();
+    return "Employee Details Updated";
+   }
+
+   deleteEmployee(empId : number){
+    this.http.delete<employee>(this.url+"/"+empId).subscribe();
+    return "Employee Details Deleted";
+   }
+
+   findAllEmployee(){
+    this.http.get<employee[]>(this.url).subscribe(data => this.employeeArr = data);
+    return this.employeeArr;
+   }
+
+   findEmployee(empId : number){
+    this.http.get<employee>(this.url+"/"+empId).subscribe(data => this.employee = data);
+    return this.employee;
+   }
 }
